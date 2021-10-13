633df976f607d8523eb7480ac7b27f9537676753
JPB <jean_paul_blanc@hotmail.com>
2021-10-13 06:04:54 +0200

    Add  Exception

--- TG_Tpl_01_Params/ConsoleWithParams.cs
+++ TG_Tpl_01_Params/ConsoleWithParams.cs
@@ -7,21 +7,30 @@ namespace TG_Tpl_01_Params
   {
     static int Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
-      AppParams appParams;
-      Dictionary<string,string> cmdLineParams;
-      GetManagedParams (args, out appParams, out cmdLineParams);
+      int rc = 0;
+      try
+      {
+        Console.WriteLine("Hello World!");
+        AppParams appParams;
+        Dictionary<string,string> cmdLineParams;
+        GetManagedParams (args, out appParams, out cmdLineParams);
 
-      Console.WriteLine($"Default value : {appParams.defaultValue}");
-      foreach(var usefullObject in appParams.usefullObjects){
-        Console.WriteLine($"\tUsefull object name : {usefullObject.Name}");
+        Console.WriteLine($"Default value : {appParams.defaultValue}");
+        foreach(var usefullObject in appParams.usefullObjects){
+          Console.WriteLine($"\tUsefull object name : {usefullObject.Name}");
+        }
+
+        foreach(var key in cmdLineParams.Keys){
+          Console.WriteLine($"{key} : {cmdLineParams[key]}");
+        }
+      }
+      catch(Exception e)
+      {
+        Console.WriteLine(e.Message);
+        rc = -1;
       }
 
-      foreach(var key in cmdLineParams.Keys){
-        Console.WriteLine($"{key} : {cmdLineParams[key]}");
-      }
-
-      return 0;
+      return rc;
     }
   }
 }
